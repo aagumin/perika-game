d46ab0a0fc72cd0bac39fd5d6ed10db1d2b3da0f
Arsen Gumin <aagumin@sberbank.ru>
2024-08-13 17:53:25 +0300

    feat: project structure

--- perika/engines/protocol.py
+++ perika/engines/protocol.py
@@ -1,6 +1,16 @@
-from typing import runtime_checkable
+from typing import runtime_checkable, Protocol
+
+from perika.game.level.complexity.protocol import LevelComplexity
 
 
-@runtime_checkable
-class Text:
+class TaskTextEngigne(Protocol):
     pass
+@runtime_checkable
+class TaskText(Protocol):
+
+    def __init__(self, text: str):
+        self.text_backend = TaskTextEngigne
+        self.complexity: LevelComplexity
+
+    def get_or_create(self):
+        pass
